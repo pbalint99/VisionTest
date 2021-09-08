fbec5ec28a76ba28455afc16081eab33b052998c
pbalint99 <41841698+pbalint99@users.noreply.github.com>
2021-09-08 15:22:36 +0200

    Contrast modified, 4 direction options

--- app/src/main/java/hu/bme/mogi/android/visiontest/activities/ContrastTestActivity.kt
+++ app/src/main/java/hu/bme/mogi/android/visiontest/activities/ContrastTestActivity.kt
@@ -4,7 +4,6 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -12,46 +11,92 @@ import hu.bme.mogi.android.visiontest.R
 import kotlinx.android.synthetic.main.activity_contrasttest.*
 import kotlinx.android.synthetic.main.activity_vatest.*
 import java.util.*
-import kotlin.math.roundToInt
 
 
 class ContrastTestActivity: AppCompatActivity() {
     var tryNumber: Int = 1
-    var results: FloatArray = floatArrayOf(0f,0f,0f)
+    var results: FloatArray = floatArrayOf(0f, 0f, 0f)
     var firstpressed: Boolean = false
+    var widthMiddle = 0; var heightMiddle = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contrasttest)
 
         gaussView.setImageResource(R.drawable.sin_feathered)
         gaussView.alpha=0f
-        nextButton.visibility= View.INVISIBLE
 
         plusButton.setOnClickListener{
             if(!firstpressed) {
                 plusButton.text="+"
-                nextButton.visibility=View.VISIBLE
                 moveGauss()
-                rotate()
-                noiseView.setImageBitmap(applyFleaEffect(BitmapFactory.decodeResource(applicationContext.resources,R.drawable.black_square)))
+                noiseView.setImageBitmap(
+                    applyFleaEffect(
+                        BitmapFactory.decodeResource(
+                            applicationContext.resources,
+                            R.drawable.black_square
+                        )
+                    )
+                )
                 firstpressed=true
             }
             gaussView.alpha+=0.01f
         }
 
-        nextButton.setOnClickListener{
-            if(tryNumber<3) {
-                noiseView.setImageBitmap(applyFleaEffect(BitmapFactory.decodeResource(applicationContext.resources,R.drawable.black_square)))
-                moveGauss()
-                rotate()
-                results[tryNumber-1]=gaussView.alpha
-                gaussView.alpha = 0.02f
-                tryNumber++
-            } else {
-                Toast.makeText(applicationContext,"Your result: "+(results.average()*100).roundToInt().toString()+"%",Toast.LENGTH_LONG).show()
-                finish()
-            }
+        //Button Listeners:
+        upBtnC.setOnClickListener{
+            guess(0)
+            moveGauss() //TODO: ne hagyd benne
         }
+        rightBtnC.setOnClickListener{
+            guess(1)
+        }
+        downBtnC.setOnClickListener{
+            guess(2)
+        }
+        leftBtnC.setOnClickListener{
+            guess(3)
+        }
+
+//        nextButton.setOnClickListener{
+//            if(tryNumber<3) {
+//                noiseView.setImageBitmap(applyFleaEffect(BitmapFactory.decodeResource(applicationContext.resources,R.drawable.black_square)))
+//                moveGauss()
+//                results[tryNumber-1]=gaussView.alpha
+//                gaussView.alpha = 0.02f
+//                tryNumber++
+//            } else {
+//                Toast.makeText(applicationContext,"Your result: "+(results.average()*100).roundToInt().toString()+"%",Toast.LENGTH_LONG).show()
+//                finish()
+//            }
+//        }
+
+//        gaussView.pivotX = gaussView.width.toFloat() / 2;
+//        gaussView.pivotY = gaussView.height.toFloat()/ 2;
+//        gaussView.setImageBitmap(
+//            setFrequency(
+//                BitmapFactory.decodeResource(
+//                    applicationContext.resources,
+//                    R.drawable.black_square
+//                )
+//            )
+//        )
+
+    }
+
+    private fun setFrequency(source: Bitmap): Bitmap? {
+        // get source image size
+        val width = source.width
+        val height = source.height
+        val pixels = IntArray(width * height)
+        var hsv: FloatArray
+        // get pixel array from source
+        source.getPixels(pixels, 0, width, 0, 0, width, height)
+
+
+        val bmOut = Bitmap.createBitmap(width, height, source.config)
+        bmOut.setPixels(pixels, 0, width, 0, 0, width, height)
+        return bmOut
     }
 
 
@@ -76,7 +121,7 @@ class ContrastTestActivity: AppCompatActivity() {
 //                    random.nextInt(255),
 //                    random.nextInt(255), random.nextInt(255)
 //                )
-                hsv= floatArrayOf(0f,0f,random.nextFloat())
+                hsv= floatArrayOf(0f, 0f, random.nextFloat())
                 val randColor: Int = Color.HSVToColor(hsv)
                 // OR
                 pixels[index] = pixels[index] or randColor
@@ -91,18 +136,44 @@ class ContrastTestActivity: AppCompatActivity() {
     private fun moveGauss() {
         val params = gaussView.layoutParams as ConstraintLayout.LayoutParams
         val random = Random()
+        val loc =  IntArray(2)
+        widthMiddle = (noiseView.width - gaussView.width)/2
+        heightMiddle = (noiseView.height - gaussView.height)/2
+
+        when((0..3).random()) {
+            0 -> {
+                loc[0] = widthMiddle
+                loc[1] = 0
+            } //Up
+            1 -> {
+                loc[0] = noiseView.width - gaussView.width
+                loc[1] = heightMiddle
+            } //Right
+            2 -> {
+                loc[0] = widthMiddle
+                loc[1] = noiseView.height - gaussView.height
+            } //Down
+            else -> {
+                loc[0] = 0
+                loc[1] = heightMiddle
+            } //Left
+        }
         params.setMargins(
-            random.nextInt(noiseView.width) - gaussView.width,
-            random.nextInt(noiseView.height) - gaussView.height,
+//            random.nextInt(noiseView.width) - gaussView.width,
+//            random.nextInt(noiseView.height) - gaussView.height,
+            loc[0],
+            loc[1],
             0,
             0
         )
         gaussView.layoutParams = params
+        gaussView.rotation=random.nextInt(360).toFloat()
     }
 
-    private fun rotate() {
-        val random = Random()
-        gaussView.rotation=random.nextInt(360).toFloat()
+    //TODO: lépcsős fentről-lenntről megbecsülés
+    private fun guess(dir: Int) {
+//        var min: Int = 100;
+//        Toast.makeText(applicationContext,"x: "+(gaussView.pivotX/noiseView.width).toString()+", y: "+(gaussView.pivotY/noiseView.height).toString(),Toast.LENGTH_SHORT).show();
     }
 
 }

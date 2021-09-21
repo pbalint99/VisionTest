448c2a80a777cae0b0459171533ee61f6b1453e6
pbalint99 <41841698+pbalint99@users.noreply.github.com>
2021-09-21 22:24:23 +0200

    Color perception way better

--- app/src/main/java/hu/bme/mogi/android/visiontest/Noise.kt
+++ app/src/main/java/hu/bme/mogi/android/visiontest/Noise.kt
@@ -3,18 +3,28 @@ package hu.bme.mogi.android.visiontest
 import android.graphics.*
 import android.widget.FrameLayout
 import android.widget.Toast
+import androidx.core.graphics.alpha
+import androidx.core.graphics.blue
+import androidx.core.graphics.green
+import androidx.core.graphics.red
 import java.util.*
 import kotlin.math.abs
 import kotlin.math.pow
 
 
 object Noise {
-    fun applyNoise(source: Bitmap, width: Int, height: Int, color: Float = 0f): Bitmap {
+    const val colorWiggleRoom = 70f
+    const val satWiggleRoom = 0.5f
+    const val circleColorDensity = 0.4f
+    fun applyNoise(source: Bitmap, width: Int, height: Int, color: Float = -1f): Bitmap {
         // get source image size
         val pixels = IntArray(width * height)
         var hsv: FloatArray = floatArrayOf(0f, 0f, 0f)
         // get pixel array from source
         source.getPixels(pixels, 0, width, 0, 0, width, height)
+        //if color is net set, set saturation to 0
+        val sat = if(color == -1f) 0f
+        else 1f
         // create a random object
         val random = Random()
         var index = 0
@@ -23,7 +33,7 @@ object Noise {
             for (x in 0 until width) {
                 // get current index in 2D-matrix
                 index = y * width + x
-                hsv= floatArrayOf(color, 1f, random.nextFloat())
+                hsv= floatArrayOf(color-colorWiggleRoom/2+colorWiggleRoom*random.nextFloat(), sat - satWiggleRoom*random.nextFloat(), random.nextFloat())
                 pixels[index] = Color.HSVToColor(hsv)
             }
         }
@@ -36,25 +46,29 @@ object Noise {
     fun applyNoiseCircle(source: Bitmap, width: Int, color: Float, bgColor: Float = 0f): Bitmap {
         // get source image size
         val pixels = IntArray(width * width)
-        var hsv: FloatArray = floatArrayOf(0f, 0f, 0f)
+        var hsv: FloatArray
+        var finalColor: Int
         // get pixel array from source
         source.getPixels(pixels, 0, width, 0, 0, width, width)
         // create a random object
         val random = Random()
         var index = 0
-        val innerD = 0.8*width //innerDiameter
         // iteration through pixels
         for (y in 0 until width) {
             for (x in 0 until width) {
                 // get current index in 2D-matrix
                 index = y * width + x
                 //TODO: optimise this
-                if(x * x - x*width + y * y - y*width < -width*width/4){
-                    if(random.nextInt()%2==0) hsv= floatArrayOf(bgColor, 1f, random.nextFloat())
-                    else hsv= floatArrayOf(color, 1f, random.nextFloat())
+                if(x * x - x * width + y * y - y*width < - width*width/4) {
+                    if(random.nextFloat()> circleColorDensity) finalColor =  Color.TRANSPARENT
+                    else {
+                        hsv= floatArrayOf(color - colorWiggleRoom/2 + colorWiggleRoom*random.nextFloat(), 1f - satWiggleRoom*random.nextFloat(), random.nextFloat())
+                        finalColor =  Color.HSVToColor(hsv)
+                    }
+                } else {
+                    finalColor = Color.TRANSPARENT
                 }
-                else hsv= floatArrayOf(bgColor, 1f, random.nextFloat())
-                pixels[index] = Color.HSVToColor(hsv)
+                pixels[index] = finalColor
             }
         }
         // output bitmap
--- app/src/main/java/hu/bme/mogi/android/visiontest/activities/ColorActivity.kt
+++ app/src/main/java/hu/bme/mogi/android/visiontest/activities/ColorActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.*
 import android.os.Build
 import android.os.Bundle
 import android.util.DisplayMetrics
+import android.widget.SeekBar
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
@@ -18,26 +19,45 @@ import kotlinx.android.synthetic.main.activity_contrasttest.plusButton
 import kotlinx.android.synthetic.main.activity_contrasttest.rightBtnC
 import kotlinx.android.synthetic.main.activity_contrasttest.upBtnC
 
-
+//TODO: background, dot unnecessary
 class ColorActivity: AppCompatActivity() {
     var bgColor = 90f
     var dotColor = 360f
     var prevDir = 5
+    var level = 0
+    var results = BooleanArray(3)
+    private var aspectRatio = 0
+    private var noiseDensity = 500
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_color)
+        upBtnC.isEnabled = false
+        downBtnC.isEnabled = false
+        leftBtnC.isEnabled = false
+        rightBtnC.isEnabled = false
+
+        //Set sizes
+        val params = dotView.layoutParams
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        val dotWidth = displayMetrics.widthPixels/5
+        aspectRatio = displayMetrics.heightPixels/displayMetrics.widthPixels
+        params.width = dotWidth
+        params.height = dotWidth
 
         plusButton.setOnClickListener {
             plusButton.isEnabled = false
-            ViewMover.move(dotView, noiseView, false)
-            noiseView.setImageBitmap(
-                Noise.applyNoise(
-                    BitmapFactory.decodeResource(
-                        applicationContext.resources, R.drawable.black_square
-                    ), 300, 600, bgColor
-                )
-            )
+            plusButton.text = ""
+            plusButton.setBackgroundColor(Color.TRANSPARENT)
+
+            upBtnC.isEnabled = true
+            downBtnC.isEnabled = true
+            leftBtnC.isEnabled = true
+            rightBtnC.isEnabled = true
+            prevDir = ViewMover.move(dotView, noiseView, false)
+
+            applyNoises()
         }
 
         //Button Listeners:
@@ -53,19 +73,66 @@ class ColorActivity: AppCompatActivity() {
         leftBtnC.setOnClickListener{
             guess(3)
         }
+        //Seekbars:
+//        seekBar?.setOnSeekBarChangeListener(object :
+//            SeekBar.OnSeekBarChangeListener {
+//            override fun onProgressChanged(seek: SeekBar,
+//                                           progress: Int, fromUser: Boolean) {
+//                // write custom code for progress is changed
+//            }
+//
+//            override fun onStartTrackingTouch(seek: SeekBar) {
+//                // write custom code for progress is started
+//            }
+//
+//            override fun onStopTrackingTouch(seek: SeekBar) {
+//                // write custom code for progress is stopped
+//                bgColor=60f+seekBar.progress
+//                noiseView.setImageBitmap(
+//                    Noise.applyNoise(
+//                        BitmapFactory.decodeResource(
+//                            applicationContext.resources, R.drawable.background
+//                        ), noiseDensity, noiseDensity*aspectRatio, bgColor
+//                    )
+//                )
+//                dotView.setImageBitmap(Noise.applyNoiseCircle(
+//                    BitmapFactory.decodeResource(
+//                        applicationContext.resources, R.drawable.dot
+//                    ), noiseDensity/10, dotColor, bgColor
+//                ))
+//                ViewMover.move(dotView, noiseView, false)
+//            }
+//        })
+//        seekBar2?.setOnSeekBarChangeListener(object :
+//            SeekBar.OnSeekBarChangeListener {
+//            override fun onProgressChanged(seek: SeekBar,
+//                                           progress: Int, fromUser: Boolean) {
+//                // write custom code for progress is changed
+//            }
+//
+//            override fun onStartTrackingTouch(seek: SeekBar) {
+//                // write custom code for progress is started
+//            }
+//
+//            override fun onStopTrackingTouch(seek: SeekBar) {
+//                // write custom code for progress is stopped
+//                dotColor=260f+seekBar.progress
+//                noiseView.setImageBitmap(
+//                    Noise.applyNoise(
+//                        BitmapFactory.decodeResource(
+//                            applicationContext.resources, R.drawable.background
+//                        ), noiseDensity, noiseDensity*aspectRatio, bgColor
+//                    )
+//                )
+//                dotView.setImageBitmap(Noise.applyNoiseCircle(
+//                    BitmapFactory.decodeResource(
+//                        applicationContext.resources, R.drawable.dot
+//                    ), noiseDensity/10, dotColor, bgColor
+//                ))
+//                ViewMover.move(dotView, noiseView, false)
+//            }
+//        })
 
-        val params = dotView.layoutParams
-        val displayMetrics = DisplayMetrics()
-        windowManager.defaultDisplay.getMetrics(displayMetrics)
-        val dotWidth = displayMetrics.widthPixels/5
-        params.width = dotWidth
-        params.height = dotWidth
-
-        dotView.setImageBitmap(Noise.applyNoiseCircle(
-            BitmapFactory.decodeResource(
-                applicationContext.resources, R.drawable.black_square
-            ), 60, dotColor, bgColor
-        ))
 
     }
 
@@ -73,26 +140,43 @@ class ColorActivity: AppCompatActivity() {
 
     //TODO: Ötlet: hol NINCS pötty?
     private fun guess(dir: Int) {
-        if (dir == prevDir) Toast.makeText(applicationContext,"correct",Toast.LENGTH_SHORT).show()
+        results[level] = dir == prevDir
+
+        if(level>1) {
+            evaluate()
+            return
+        }
 
         prevDir = ViewMover.move(dotView, noiseView, false)
 
+        applyNoises()
+
+        level++
+    }
+
+    private fun evaluate() {
+        var correct = 0
+        for (i in 0..2) {
+            if (results[i]) correct++
+        }
+        Toast.makeText(applicationContext,"Your result is: "+correct+"/3",Toast.LENGTH_SHORT).show()
+        finish()
+    }
+
+    private fun applyNoises() {
         noiseView.setImageBitmap(
             Noise.applyNoise(
                 BitmapFactory.decodeResource(
                     applicationContext.resources, R.drawable.black_square
-                ), 300, 600, bgColor
+                ), noiseDensity, noiseDensity*aspectRatio, bgColor
             )
         )
         dotView.setImageBitmap(
             Noise.applyNoiseCircle(
                 BitmapFactory.decodeResource(
                     applicationContext.resources, R.drawable.black_square
-            ), 60,dotColor,bgColor
-        ))
-
-
+                ), noiseDensity/8, dotColor, bgColor
+            )
+        )
     }
-
-
 }
